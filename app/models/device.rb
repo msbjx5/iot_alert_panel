ce21f38d1e286852ce311fff668a0c667f5d0ff4
Matheus A.M <matheusthebr@gmail.com>
2019-10-06 14:54:41 -0300

    Fix rubocop ofenses.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 class Device < ApplicationRecord
   validates :name, :mac, :cel, presence: true
+
+  def send_alert
+    client = Twilio::REST::Client.new
+    client.messages.create({
+      from: TWILIO['twilio_phone_number'],
+      to: cel,
+      body: "ALERTA #{name.upcase}: Alguém no carro!"
+    })
+  end
 end
