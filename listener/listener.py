bc25b1597206bf04380c318a52a90239ecfd7c0d
msbjx5 <msbjx5@gmail.com>
2019-10-09 01:02:20 -0300

    listener.py fix

--- listener/listener.py
+++ listener/listener.py
@@ -1,29 +1,32 @@
-import datetime
-import logging
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
+import datetime
 
-def persists(msg):
-    json_body = [
-        {
-            "measurement": "metrics",
-            "tags": {},
-            "time": datetime.datetime.utcnow().isoformat(),
-            "fields": {
-                "value": int(msg.payload)
-            }
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("metrics/#")
+
+def on_message(client, userdata, msg):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    json_body = [ 
+    {
+        "measurement": "metrics",
+        "tags": {
+            "host": "esp32",
+        },
+        "fields": {
+            "value": str(msg.payload)
         }
+    }
     ]
-    logging.info(json_body)
     influx_client.write_points(json_body)
-
-logging.basicConfig(level=logging.DEBUG) #INFO)
+    print(msg.topic+" "+str(msg.payload))
 
 influx_client = InfluxDBClient('influx', 8086, database='metrics')
 client = mqtt.Client()
-
-client.on_connect = lambda self, mosq, obj, rc: self.subscribe("/metrics")
-client.on_message = lambda client, userdata, msg: persists(msg)
+client.on_connect = on_connect
+client.on_message = on_message
 
 client.connect("mosquitto", 1883, 60)
+
 client.loop_forever()
